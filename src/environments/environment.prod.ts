033261f4e85ec560b1694f5b14e07343723e66d5
Jainam Sheth <j4inam@gmail.com>
2020-04-04 23:42:21 -0400

    Configure Firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  HOST: 'https://api.openbrewerydb.org/breweries',
+  firebaseConfig: {
+    apiKey: "AIzaSyB_DLePDqgMU68fpaMNt3RJ-fiAPuBzITc",
+    authDomain: "ngx-table-filters.firebaseapp.com",
+    databaseURL: "https://ngx-table-filters.firebaseio.com",
+    projectId: "ngx-table-filters",
+    storageBucket: "ngx-table-filters.appspot.com",
+    messagingSenderId: "502573949021",
+    appId: "1:502573949021:web:92ae776036f383c8f2adb4",
+    measurementId: "G-JVECX319YN"
+  }
 };
